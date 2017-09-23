667a8d4614a3ad1dcb197bbec84bbe79091bb8e6
Axel Clark <axelclark2@yahoo.com>
2017-09-23 18:17:08 -0400

    Create lights firmware project

---
+++ lights_firmware/lib/lights_firmware.ex
@@ -0,0 +1,3 @@
+defmodule LightsFirmware do
+  
+end
---
+++ lights_firmware/test/lights_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule LightsFirmwareTest do
+  use ExUnit.Case
+  doctest LightsFirmware
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
