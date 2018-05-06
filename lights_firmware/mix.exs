d3a5bc804ca97d01691ff37485072cbd48a008b8
Axel Clark <axelclark2@yahoo.com>
2018-05-05 22:00:34 -0400

    Update Nerves to 1.0 & Elixir/Erlang

--- lights_firmware/mix.exs
+++ lights_firmware/mix.exs
@@ -3,56 +3,38 @@ defmodule LightsFirmware.Mixfile do
 
   @target System.get_env("MIX_TARGET") || "host"
 
-  Mix.shell.info([:green, """
-  Mix environment
-    MIX_TARGET:   #{@target}
-    MIX_ENV:      #{Mix.env}
-  """, :reset])
-
   def project do
-    [app: :lights_firmware,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     target: @target,
-     archives: [nerves_bootstrap: "~> 0.6"],
-     deps_path: "deps/#{@target}",
-     build_path: "_build/#{@target}",
-     lockfile: "mix.lock.#{@target}",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(@target),
-     deps: deps()]
+    [
+      app: :lights_firmware,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      target: @target,
+      archives: [nerves_bootstrap: "~> 1.0"],
+      deps_path: "deps/#{@target}",
+      build_path: "_build/#{@target}",
+      lockfile: "mix.lock.#{@target}",
+      start_permanent: Mix.env() == :prod,
+      aliases: [loadconfig: [&bootstrap/1]],
+      deps: deps()
+    ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
-  def application, do: application(@target)
-
-  # Specify target specific application configurations
-  # It is common that the application start function will start and supervise
-  # applications which could cause the host to fail. Because of this, we only
-  # invoke LightsFirmware.start/2 when running on a target.
-  def application("host") do
-    [extra_applications: [:logger]]
+  # Starting nerves_bootstrap adds the required aliases to Mix.Project.config()
+  # Aliases are only added if MIX_TARGET is set.
+  def bootstrap(args) do
+    Application.start(:nerves_bootstrap)
+    Mix.Task.run("loadconfig", args)
   end
-  def application(_target) do
+
+
+  def application do
     [mod: {LightsFirmware.Application, []},
      extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   def deps do
     [
-      {:nerves, "~> 0.7", runtime: false},
+      {:nerves, "~> 1.0", runtime: false},
     ] ++ deps(@target)
   end
 
@@ -67,15 +49,15 @@ defmodule LightsFirmware.Mixfile do
     ] ++ system(target)
   end
 
-  def system("rpi"), do: [{:nerves_system_rpi, ">= 0.0.0", runtime: false}]
-  def system("rpi0"), do: [{:nerves_system_rpi0, ">= 0.0.0", runtime: false}]
-  def system("rpi2"), do: [{:nerves_system_rpi2, ">= 0.0.0", runtime: false}]
-  def system("rpi3"), do: [{:nerves_system_rpi3, ">= 0.0.0", runtime: false}]
-  def system("bbb"), do: [{:nerves_system_bbb, ">= 0.0.0", runtime: false}]
-  def system("linkit"), do: [{:nerves_system_linkit, ">= 0.0.0", runtime: false}]
-  def system("ev3"), do: [{:nerves_system_ev3, ">= 0.0.0", runtime: false}]
-  def system("qemu_arm"), do: [{:nerves_system_qemu_arm, ">= 0.0.0", runtime: false}]
-  def system(target), do: Mix.raise "Unknown MIX_TARGET: #{target}"
+  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.0", runtime: false}]
+  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.0", runtime: false}]
+  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0", runtime: false}]
+  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.0", runtime: false}]
+  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0", runtime: false}]
+  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0", runtime: false}]
+  defp system("qemu_arm"), do: [{:nerves_system_qemu_arm, "~> 1.0", runtime: false}]
+  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0", runtime: false}]
+  defp system(target), do: Mix.raise "Unknown MIX_TARGET: #{target}"
 
   # We do not invoke the Nerves Env when running on the Host
   def aliases("host"), do: []
